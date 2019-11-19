aeb739578efede84d97cc0057aa3ade04988febe
Wing Tching LIM <wtlim@vente-privee.com>
2019-11-19 14:18:00 +0100

    Add FireBase

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # Calendrier de l'Avent 2019
 
+--------------------------------------------
+
+https://calendar-9f1c9.web.app/
+
+--------------------------------------------
+
+Pour déployer :
+$> elm-app build
+$> firebase deploy
+
+--------------------------------------------
+
+FireBase :
+
+$> npm install -g firebase-tools
+$> firebase login     
+$> firebase init     
+$> firebase deploy            
+
+-------------------------------------------
+
 Try this to get v0.19.1
 $> npm install elm@0.19.1 -d
+
+-------------------------------------------
---
+++ build/precache-manifest.03e4dd9cd5988071b1bd68938d3e9996.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a27a4163254fc8fce870c8cc3a3f94f",
+    "url": "/404.html"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "1fe69b2707fda1d5d2b3d6697426f24e",
+    "url": "/images/calendar.mic"
+  },
+  {
+    "revision": "5fc692aa3276de3ad01bcf8fbc092bca",
+    "url": "/images/door/door1.jpg"
+  },
+  {
+    "revision": "bd24aa89327bb4e08753167cb1505c51",
+    "url": "/images/door/door10.jpg"
+  },
+  {
+    "revision": "bf67b8b41135fcb1a93678ac2a992338",
+    "url": "/images/door/door11.jpg"
+  },
+  {
+    "revision": "ecdb741aa8a0ff61be9eb5182abe5fb8",
+    "url": "/images/door/door12.jpg"
+  },
+  {
+    "revision": "eae364195465088a2d83bc254d17b9cb",
+    "url": "/images/door/door13.jpg"
+  },
+  {
+    "revision": "c60a834c0d5bdbe922e38be9cb0e0e88",
+    "url": "/images/door/door14.jpg"
+  },
+  {
+    "revision": "05280ad55b8ef3d078d715c0502bec64",
+    "url": "/images/door/door15.jpg"
+  },
+  {
+    "revision": "28962b1a0286a4fe34dfbf2a0306c73a",
+    "url": "/images/door/door16.jpg"
+  },
+  {
+    "revision": "b701ed01e094374b38b5528bcd380983",
+    "url": "/images/door/door17.jpg"
+  },
+  {
+    "revision": "0ba778eee0e498f4b2993b3fb202c842",
+    "url": "/images/door/door18.jpg"
+  },
+  {
+    "revision": "f8de23067658130026ac89fb3b214ab7",
+    "url": "/images/door/door19.jpg"
+  },
+  {
+    "revision": "069881df905572625391e4b774f0708f",
+    "url": "/images/door/door2.jpg"
+  },
+  {
+    "revision": "46a0f9be83178fca1a6d05192e904ec5",
+    "url": "/images/door/door20.jpg"
+  },
+  {
+    "revision": "d8e6114a2594fdbe0c79386ad9eea676",
+    "url": "/images/door/door21.jpg"
+  },
+  {
+    "revision": "d62296183b05d53d2d95b38c19bc57a1",
+    "url": "/images/door/door22.jpg"
+  },
+  {
+    "revision": "4c05712227ceae691f7708fc0c0fd06e",
+    "url": "/images/door/door23.jpg"
+  },
+  {
+    "revision": "7515d97dd134b6ed1184829235716252",
+    "url": "/images/door/door24.jpg"
+  },
+  {
+    "revision": "3f629cd1c6a7b82e8df4f773ab7dafdb",
+    "url": "/images/door/door25.jpg"
+  },
+  {
+    "revision": "5a3a1c90d98543e589d2a7e0a8b65e12",
+    "url": "/images/door/door3.jpg"
+  },
+  {
+    "revision": "b501d931bc72721fb474b31fb63ac5b9",
+    "url": "/images/door/door4.jpg"
+  },
+  {
+    "revision": "be5ebdb4fc63cf2e63cbc076f2fde503",
+    "url": "/images/door/door5.jpg"
+  },
+  {
+    "revision": "7eae88b03d83f7a845465eb1b30091df",
+    "url": "/images/door/door6.jpg"
+  },
+  {
+    "revision": "d9607b8bb869df872ae28c4fb992d5ba",
+    "url": "/images/door/door7.jpg"
+  },
+  {
+    "revision": "d471b2a34c4d6fde845df1cbe61e429d",
+    "url": "/images/door/door8.jpg"
+  },
+  {
+    "revision": "3c07bdb66cccb60e16df4d68db30b5c3",
+    "url": "/images/door/door9.jpg"
+  },
+  {
+    "revision": "e825aa1f75ef8d93568e52ba99b8b8ec",
+    "url": "/images/img/1.jpg"
+  },
+  {
+    "revision": "b13065bb7ac0ccc35f981bcdb4cb9719",
+    "url": "/images/img/10.jpg"
+  },
+  {
+    "revision": "7b0afda46225d99edba6bf6259980cd1",
+    "url": "/images/img/11.jpg"
+  },
+  {
+    "revision": "fbd5752b1719dfefe15613ecbcacac17",
+    "url": "/images/img/12.jpg"
+  },
+  {
+    "revision": "1eb2bcda38c98b37d19b51f62c6955b5",
+    "url": "/images/img/13.jpg"
+  },
+  {
+    "revision": "8d5d682ab1ecc114f9d57883b2188e21",
+    "url": "/images/img/14.jpg"
+  },
+  {
+    "revision": "ab3bfd0da29a7506ed631bddac1facbd",
+    "url": "/images/img/15.jpg"
+  },
+  {
+    "revision": "037d13b5793fb8f66e40bcf8ba1bb236",
+    "url": "/images/img/16.jpg"
+  },
+  {
+    "revision": "861a6bf380f8a4f0e4a5f6365aa98f90",
+    "url": "/images/img/17.jpg"
+  },
+  {
+    "revision": "c0f5ad414eb8e63c6c2b346ce406d336",
+    "url": "/images/img/18.jpg"
+  },
+  {
+    "revision": "6b1370bb0d0b76a6a8837d4d6c5e8bd8",
+    "url": "/images/img/19.jpg"
+  },
+  {
+    "revision": "6ce1166a43c51637361fd607866ea8dc",
+    "url": "/images/img/2.jpg"
+  },
+  {
+    "revision": "cd3cb7e18e992fb9fdaa959a10923d2a",
+    "url": "/images/img/20.jpg"
+  },
+  {
+    "revision": "3ccb133ed588cff101774adc976a2c6b",
+    "url": "/images/img/21.jpg"
+  },
+  {
+    "revision": "379eb288f5ee49fbb8931c0505513baf",
+    "url": "/images/img/22.jpg"
+  },
+  {
+    "revision": "1ae35b166d7f7a0f4cf24775d8d85ea9",
+    "url": "/images/img/23.jpg"
+  },
+  {
+    "revision": "264a6649d0dd0f95eaa423c83e9522f8",
+    "url": "/images/img/24.jpg"
+  },
+  {
+    "revision": "57cb4c6fe64eae04daa2196431152c6c",
+    "url": "/images/img/25.jpg"
+  },
+  {
+    "revision": "b5839b9ce571d92135c66684d0399249",
+    "url": "/images/img/3.jpg"
+  },
+  {
+    "revision": "70d785df13eef6129d2ebd1548b8354d",
+    "url": "/images/img/4.jpg"
+  },
+  {
+    "revision": "b24ac8ad54a8a761030cc10819e35dee",
+    "url": "/images/img/5.jpg"
+  },
+  {
+    "revision": "2b9e8f3a8561115758c65b8aef67efa9",
+    "url": "/images/img/6.jpg"
+  },
+  {
+    "revision": "a98832bd4f57fdbee646db5754b66330",
+    "url": "/images/img/7.jpg"
+  },
+  {
+    "revision": "4b7a818ba5f1b5f2003dff8a01c9e43d",
+    "url": "/images/img/8.jpg"
+  },
+  {
+    "revision": "4650c4dd303c0c2c001623b079431f84",
+    "url": "/images/img/9.jpg"
+  },
+  {
+    "revision": "880b6f331e5d65e9f146b29aa39969d0",
+    "url": "/images/wallpaper.jpg"
+  },
+  {
+    "revision": "aaa8fdd21df49a1933e7776c704a77fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "d64dd95399f0d3c6c45321f1771db92a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4bfcfcf308f00b96066c",
+    "url": "/static/css/main.8e5a92e6.chunk.css"
+  },
+  {
+    "revision": "4bfcfcf308f00b96066c",
+    "url": "/static/js/main.4bfcfcf3.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
